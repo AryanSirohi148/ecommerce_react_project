138b11c0bd67fc075630857c46cd0484b98edef0
Aryan Sirohi <aryansirohi@Aryans-MacBook-Air.local>
2025-04-19 17:00:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='text-3xl font-bold underline' >
+     hello          
+   </div>
+  )
+}
+
+export default App
