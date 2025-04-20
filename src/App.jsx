9ca281bf668bba107fb0c0f19b79fd5d0129f213
Aryan Sirohi <aryansirohi@Aryans-MacBook-Air.local>
2025-04-20 12:17:07 +0530

    Add Navbar component for eCommerce site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
-   <div className='text-3xl font-bold underline' >
-     hello          
+   <div>
+    <Navbar/>        
    </div>
   )
 }
-
+ 
 export default App
